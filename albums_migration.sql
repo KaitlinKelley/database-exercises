8677be86e9167a64047c4fcbc7f689d92a2b05c7
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-01-11 11:37:07 -0600

    finished first table exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
-    release_date INT(4) NOT NULL,
-    sales FLOAT
-
+    release_date INT(4) UNSIGNED NOT NULL,
+    sales FLOAT(10,2),
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
 );
