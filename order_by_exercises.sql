d396c5ba3e5a27e87f964dc51bf7ebb2bf92c66c
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-01-13 22:10:58 -0600

    completed order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,9 +1,12 @@
 USE employees;
 
+DESCRIBE employees;
+
 #709 rows
 SELECT first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
 
 #refactor to use OR, 709 rows
 SELECT first_name, last_name
@@ -53,16 +56,24 @@ FROM employees
 WHERE last_name LIKE 'E%'
   AND last_name LIKE '%e';
 
+#899 rows, should be same as above
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
 #362 rows
 SELECT first_name, last_name, hire_date, birth_date
 FROM employees
 WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-  AND (birth_date LIKE '%12-25%');
+  AND (birth_date LIKE '%12-25%')
+    ORDER BY birth_date, hire_date DESC;
 
 #547 rows
 SELECT first_name, last_name
 FROM employees
 WHERE (last_name LIKE '%q%'
     OR last_name LIKE '%Q%')
-    AND (last_name NOT LIKE '%qu%'
-     OR last_name NOT LIKE '%Qu%');
+  AND (last_name NOT LIKE '%qu%'
+    OR last_name NOT LIKE '%Qu%');
