f8ab633b049c747fe279429594d45c5bc85be2c9
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-01-14 11:40:29 -0600

    did walkthrough for exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,43 +1,43 @@
 USE employees;
 
-#7 rows
+#Exercise 2, 7 rows
 SELECT DISTINCT title
     FROM titles;
 
-#5 rows
+#Exercise 3, 5 rows
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%'
   AND last_name LIKE '%e'
     GROUP BY last_name;
 
-#846 rows
+#Exercise 4, 846 rows
 SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%'
   AND last_name LIKE '%e'
 GROUP BY last_name, first_name;
 
-#3 rows
+#Exercise 5, 3 rows
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '%q%'
   AND last_name NOT LIKE '%qu%'
     GROUP BY last_name;
 
-# SELECT last_name, COUNT(last_name)
-# FROM employees
-# WHERE last_name LIKE 'E%'
-#   AND last_name LIKE '%e'
-#     AND last_name LIKE '%q%'
-#         AND last_name NOT LIKE '%qu%';
-
-
-#2 rows
-SELECT COUNT(*), gender
+#Exercise 6, 8 rows
+SELECT last_name, COUNT(last_name)
 FROM employees
-WHERE (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
+WHERE last_name LIKE 'E%e'
+    OR (last_name LIKE '%q%'
+        AND last_name NOT LIKE '%qu%')
+        GROUP BY last_name
+        ORDER BY last_name;
+
+
+#Exercise 7, 2 rows
+SELECT COUNT(gender), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
     GROUP BY gender;
 
