9867a3bea9892e76f30f5d8dfc120a323dead9ef
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-01-18 23:15:21 -0600

    did first bonus for subqueries

--- subqueries_exercise.sql
+++ subqueries_exercise.sql
@@ -18,10 +18,36 @@ WHERE first_name IN(
 
 SELECT title
 FROM titles AS t
-         JOIN employees e ON e.emp_no = t.emp_no
+JOIN employees e ON e.emp_no = t.emp_no
 WHERE first_name IN(
     SELECT first_name
     FROM employees
     WHERE first_name = 'Aamod')
 GROUP BY title;
+
+
+
+
+SELECT e.first_name AS first_name, e.last_name AS last_name
+FROM employees AS e
+JOIN titles AS t ON t.emp_no = e.emp_no
+WHERE e.gender = 'F' AND t.title IN(
+    SELECT title
+    FROM titles
+    WHERE title = 'Manager'
+    AND t.to_date = '9999-01-01'
+    );
+
+SELECT d.dept_name AS dept_name
+FROM departments AS d
+JOIN dept_emp AS de ON d.dept_no = de.dept_no
+JOIN employees e ON de.emp_no = e.emp_no
+JOIN titles t on e.emp_no = t.emp_no
+WHERE e.gender = 'F' AND t.title IN(
+    SELECT title
+    FROM titles
+    WHERE title = 'Manager'
+    AND t.to_date = '9999-01-01'
+    )
+GROUP BY dept_name;
 
